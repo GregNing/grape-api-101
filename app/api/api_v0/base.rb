0e77fc307334063dcfd53222ebc547af8d35b6c7
Greg <GregJieNing@gmail.com>
2020-08-27 16:00:48 +0800

    製作 exception handlers

--- app/api/api_v0/base.rb
+++ app/api/api_v0/base.rb
@@ -2,6 +2,7 @@ module ApiV0
   class Base < Grape::API
     version 'v0', using: :path
     use ApiV0::Auth::Middleware
+    include ApiV0::ExceptionHandlers
 
     mount Ping
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_072136) do
+ActiveRecord::Schema.define(version: 2020_08_27_075235) do
+
+  create_table "api_access_tokens", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "key"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
